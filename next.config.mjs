23f54525f574ddb9aed47bbf0f21a864bc2842a9
Hugo Lipko <hugo.lipko@gmail.com>
2024-06-22 20:20:31 +0300

    Added next/image

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    domains: ["m.media-amazon.com"],
+    formats: ["image/avif", "image/webp"],
+  },
+};
 
 export default nextConfig;
